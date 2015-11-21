3c07bf2ec8efbc2406bd7f9488c18d68f20109de
Oliver Norton <onorton125@gmail.com>
2015-11-21 01:13:01 +0000

    Update main.cpp to include main loop and skeleton.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 int main() {
+    
+    //Create 
+    
+    //
+    
+    
+    //Main loop
+    while(true) {
+        world.updateAgents();
+        world.drawWorld();
+    }
+    
     return 0;
-
 }
 
 
