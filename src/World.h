061f1e01d8c0233c754ccdfdd0eaca7eb0900dad
Oliver Norton <onorton125@gmail.com>
2015-11-21 01:06:11 +0000

    Updated World.h to include function prototypes.

--- src/World.h
+++ src/World.h
@@ -1,7 +1,16 @@
 #ifndef WORLD_H
 #define WORLD_H
+#include <vector>
+#include "Agent.h"
+
 class World {
   int width;
   int height;
+  std::vector<Agent*> agents;
+
+public:
+  World(int width, int height, std::vector<Agent*> agents);
+  void updateAgents();
+  void drawWorld();
 };
 #endif
