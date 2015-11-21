c1038c20bad0927225b462446f162dac8856dc8b
Oliver Norton <onorton125@gmail.com>
2015-11-21 01:09:08 +0000

    Create World.cpp and implement constructor.

---
+++ src/World.cpp
@@ -0,0 +1,19 @@
+#include "World.h"
+World::World(int width, int height, std::vector<Agent*> agents) {
+  this->width = width;
+  this->height = height;
+  this->agents = agents;
+}
+
+void World::updateAgents() {
+//Update orientations and then update positions
+}
+
+void World::drawWorld() {
+//Draws the world including the agents
+}
+
+
+
+
+
