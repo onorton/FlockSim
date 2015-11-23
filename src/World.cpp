b8390d629ad8c2a9bfa01de7b5104b0593932933
Oliver Norton <onorton125@gmail.com>
2015-11-23 20:21:48 +0000

    Updated World.cpp
    Implemented updateAgents() and drawWorld().

--- src/World.cpp
+++ src/World.cpp
@@ -1,4 +1,5 @@
 #include "World.h"
+#include <iostream>
 World::World(int width, int height, std::vector<Agent*> agents) {
   this->width = width;
   this->height = height;
@@ -7,17 +8,34 @@ World::World(int width, int height, std::vector<Agent*> agents) {
 
 World::~World() {
    for (Agent* a : agents) {
-		delete a;
+	delete a;
    }
 }
 
 void World::updateAgents() {
 //Update orientations and then update positions
-
+    for (Agent* a : agents) {
+        a->updateOrientation();
+    }
+   
+    for (Agent* a : agents) {
+        a->updatePosition();
+    }
 }
 
 void World::drawWorld() {
 //Draws the world including the agents
+   for (int h = 0; h < height; h++) {
+        for (int w = 0; w < width; w++) {
+   	    for (Agent* a : agents) {
+   	    	if (a->getPosition.getX() == w && a->getPosition.getY())
+   	    	    std::cout << '@';
+   	    	else 
+   	    	    std::cout << ' ';
+   	    }	
+   	}
+   }
+
 }
 
 
